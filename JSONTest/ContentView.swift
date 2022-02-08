e4560c2a90f343e52d8fcd6b9126ae8438c48218
Code Palace <codepalace@fastmail.se>
2022-02-08 11:32:57 +0100

    Initial Commit

---
+++ JSONTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JSONTest
+//
+//  Created by Federico on 08/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JSONTest/JSONTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONTestApp.swift
+//  JSONTest
+//
+//  Created by Federico on 08/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct JSONTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
